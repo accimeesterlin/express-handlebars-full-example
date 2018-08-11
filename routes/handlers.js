392f42de941e8e988ef3afac7e71dcb06731fa59
Esterling Accime <esterling@brainbytescreative.com>
2018-08-11 15:00:42 -0400

    refactor our routes

---
+++ routes/handlers.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const router = express.Router(); 
+
+
+// Routing 
+router.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Home Page',
+        name: 'Esterling Accime',
+        age: 5,
+        isDisplayName: true,
+        isAgeEnabled: true
+    });
+});
+
+
+router.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus, eligendi eius! Qui'
+    });
+});
+
+
+
+router.get('/dashboard', (req, res) => {
+
+    res.render('dashboard', {
+        isListEnabled: true,
+        author: {
+            firstName: 'Peter',
+            lastName: 'James',
+            project: {
+                name: 'Build Random Quote'
+            }
+        }
+    });
+});
+
+router.get('/each/helper', (req, res) => {
+
+    res.render('contact', {
+        people: [
+            "James",
+            "Peter",
+            "Sadrack",
+            "Morissa"
+        ],
+        user: {
+            username: 'accimeesterlin',
+            age: 20,
+            phone: 4647644
+        },
+        lists: [
+            {
+                items: ['Mango', 'Banana', 'Pinerouterle']
+            },
+
+            {
+                items: ['Potatoe', 'Manioc', 'Avocado']
+            }
+        ]
+    });
+});
+
+
+
+router.get('/look', (req, res) => {
+
+    res.render('lookup', {
+        user: {
+            username: 'accimeesterlin',
+            age: 20,
+            phone: 4647644
+        },
+        people: [
+            "James",
+            "Peter",
+            "Sadrack",
+            "Morissa"
+        ]
+    });
+});
+
+module.exports = router;
+
+
--- server.js
+++ server.js
@@ -2,76 +2,21 @@ const express = require('express');
 const app = express();
 const expbs = require('express-handlebars');
 const path = require('path');
+const routes = require('./routes/handlers');
 
 
 app.engine('handlebars', expbs({
     defaultLayout: 'main',
-    layoutsDir: path.join(__dirname, 'views/mainLayout')
+    layoutsDir: path.join(__dirname, 'views/mainLayout'),
+    partialsDir: path.join(__dirname, 'views/pieces')
 }));
 
 app.set('view engine', 'handlebars');
 
-// Routing 
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Home Page',
-        name: 'Esterling Accime',
-        age: 5,
-        isDisplayName: true,
-        isAgeEnabled: true
-    });
-});
+
+app.use('/', routes);
 
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About Me',
-        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus, eligendi eius! Qui'
-    });
-});
-
-
-
-app.get('/dashboard', (req, res) => {
-
-    res.render('dashboard', {
-        isListEnabled: true,
-        author: {
-            firstName: 'Peter',
-            lastName: 'James',
-            project: {
-                name: 'Build Random Quote'
-            }
-        }
-    });
-});
-
-
-app.get('/contact', (req, res) => {
-    res.render('contact', {
-        people: [
-            "James",
-            "Peter",
-            "Sadrack",
-            "Morissa"
-        ], 
-        user: {
-            username: 'accimeesterlin',
-            age: 20,
-            phone: 4647644
-        },
-
-        lists: [
-            {
-                items: ['Mango', 'Banana', 'Pineapple']
-            },
-
-            {
-                items: ['Potatoe', 'Manioc', 'Avocado']
-            }
-        ]
-    });
-});
 
 app.listen(8080, () => {
     console.log('Server is starting at port ', 8080);
